a88d17657dcc3925361d4aa3fee9e5155a6b29b5
kazemicode <sarieses@gmail.com>
2018-11-11 14:52:12 -0800

    Cleaned up and added comments

--- Lab7.py
+++ Lab7.py
@@ -2,14 +2,11 @@
 # change output directory to desired path on your machine
 
 ## Output directory
-dir = ""
+#dir = ""
 
 ## main method
-def main():
-  writePict(removeRedEye(getPic(), 322, 995, 385, 465), dir + "/noredeye.jpg")
-  writePict(sepia(getPic()), dir + "/sepia.jpg")
-  writePict(artify(getPic()), dir + "/artify2.jpg")
-  writePict(chromaKey(getPic(), getPic()), dir + "/greenscreen2.png")
+#def main():
+ 
 
 
 # Returns the picture given a directory
@@ -36,28 +33,36 @@ def warmUp(pic):
     
   show(pic)
    
-def thanks(canvas):
+# the card canvas is a scene with 3 pumpkins with cutouts for faces
+def drawCard(canvas):
+  # Three pics below should be of faces to draw on cutouts
   pic1 = getPic()
   pic2 = getPic()
   pic3 = getPic()
+  
+  # Draws face on pumpkin 1
   drawFace(canvas, pic1, 181, 362, 241, 403)
+  # Draws face on pumpkin 2
   drawFace(canvas, pic2, 371, 611, 352, 521)
+  # Draws face on pumpkin 3
   drawFace(canvas, pic3, 628, 798, 266, 440)
   show(canvas)
 
-#pumpkin 1: drawFace(181, 362, 241, 403)
-#pumpkin 2: drawFace(372, 611, 352, 521)
-#pumpkin 3: drawFace(628, 798, 266, 440)
+
 def drawFace(canvas, face, x1, x2, y1, y2):
   holeWidth = abs(x1-x2)
   holeHeight = abs(y1-y2)
   faceWidth = getWidth(face)
   faceHeight = getHeight(face)
   
-# Check sizes of photos and resize if necessary
-  widthRatio = 1
-  heightRatio = 1
+
+  widthRatio, heightRatio = 1
   
+  # Check sizes of photos and resize if necessary
+  # To do: Find out how to stretch or shrink by a non-integer amount
+  # without running into errors
+  # To do: Can we generalize a function (named resize) to shrink/stretch a photo
+  # Rather than creating separate shrink/stretch functions?
   if(holeWidth > faceWidth):
     widthRatio = float(holeWidth/faceWidth)
     print widthRatio
@@ -67,17 +72,21 @@ def drawFace(canvas, face, x1, x2, y1, y2):
     print heightRatio
   stretch(face, widthRatio, heightRatio)
   
-
+# px and py are the x, y locations of the picture of a face
   px = 0
   py = 0
   
   for x in range(x1, x2):
     for y in range(y1, y2):
+      # If the pixel in the canvas is white enough, draw the pixel
+      # from the face picture. Otherwise, leave it alone!    
       if distance(getColor(getPixel(canvas, x, y)), white) < 100:
         setColor(getPixel(canvas, x, y), getColor(getPixel(face, px, py)))
-        
+      # Proceed to next y location of face picture  
       py = py + 1
         
+    # When the inner loop is exited, increase the x location of face picture
+    # And reset the y location of the face picture to 0
     px = px + 1
     py = 0
     
@@ -90,7 +99,8 @@ def stretch(pic, widthRatio, heightRatio):
   for x in range(0, getWidth(canvas)):
      for y in range(0, getHeight(canvas)):
         setColor(getPixel(canvas,x,y), getColor(getPixel(pic,x/widthRatio,y/heightRatio))) 
-  show(canvas)
+  #show(canvas)
+  return canvas
    
         
   
